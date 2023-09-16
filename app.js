9bb3156d7f853571780f6fbb3e38177b4a1e2756
Lucioo04 <luciotesta@gmail.com>
2023-09-16 16:12:07 -0300

    modifique el archivo app.js agregando un codigo

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
 
-console.log("esto es una App")
+//console.log("esto es una App")
+
+const productos = {
+    id : 1,
+    nombre : "Camiseta",
+    precio : 20,
+    colores : ["Rojo", "Azul", "Verde"],
+    especificaciones : {
+        material : "Algodon",
+        tallas : ["S","M","L"]
+    }
+};
+
+let {nombre, precio,especificaciones} = productos
+
+const productoActualizado = {
+    ...productos,
+    precio : 25,
+    colores : [...productos.colores, "Negro"], 
+    especificaciones:{
+        tallas:[...productos.especificaciones.tallas, "XL"]
+    }
+    
+
+};
+
+//console.log(nombre);
+//console.log(precio);
+//console.log(especificaciones);
+console.log(productos);
+console.log(productoActualizado);
+
