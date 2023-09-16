549c36070a41e4074790c1035287f112e0e107c9
JuanLicciardi <juanelicciardi@gmail.com>
2023-09-16 16:13:30 -0300

    Agrege dos autos al archivo autos.js

--- autos.js
+++ autos.js
@@ -1,4 +1,8 @@
 let autos = [
     {marca :'Ford',modelo:'Fiesta',precio:150000,km:200 ,color:'Azul',cuotas:12, anio:2019,patente:'APL123',vendido:false,},
-    {marca :'Toyota',modelo:'Corolla',precio:100000,km:0,color:'Blanco',cuotas:14,anio:2019,patente:'JJK116',vendido:true,}
+    {marca :'Toyota',modelo:'Corolla',precio:100000,km:0,color:'Blanco',cuotas:14,anio:2019,patente:'JJK116',vendido:true,},
+    {marca :'Fiat',modelo:'Uno',precio:150000,km:200 ,color:'Amarilla',cuotas:12, anio:2019,patente:'APL123',vendido:false,},
+    {marca :'Chevrolet',modelo:'Spin',precio:100000,km:0,color:'Verde Petroleo',cuotas:24,anio:2023,patente:'AF 325 TC',vendido:true,}
+
+
 ]
